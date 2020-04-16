ddf03c77113391dd40ffb3d05184d16d2a6167da
nsainez <nsainez@gmail.com>
2020-04-15 19:48:28 -0700

    small adjustments

--- assignment3.c
+++ assignment3.c
@@ -7,10 +7,26 @@
 #include <stdlib.h>
 #include <softPwm.h>
 
-void init(struct *motors, int enable, int forward, int reverse);
-void forward(struct *motors, int speed);
-void reverse(struct *motors, int speed);
-void stop(struct *motors);
+/* speed = duty cycle % from 0 - 100 */
+void forward(struct Motor *motors, int speed){
+        printf("Moving forward...");
+        softPwmWrite(motors -> enable_motor, speed);
+        digitalWrite(motors -> forward_motor, 0);
+        digitalWrite(motors -> reverse_motor, 1);
+}
+
+void reverse (struct Motor *motors, int speed){
+        printf("Moving in reverse...");
+        softPwmWrite(motors -> enable_motor, speed);
+        digitalWrite(motors -> forward_motor, 1);
+        digitalWrite(motors -> reverse_motor, 0);
+}
+void stop (struct Motor *motors) {
+        printf("Stopping...");
+        softPwmWrite(motors -> enable_motor, speed);
+        digitalWrite(motors -> forward_motor, 1);
+        digitalWrite(motors -> reverse_motor, 1);
+}
 
 /* DC Motor 1 : enable-11 control-13,15
    DC Motor 2 : enable-22 control-16,18 
@@ -103,23 +119,3 @@ void init(struct Motors *motors, int enable, int forward, int reverse){
 			}
 	}
 
-/* speed = duty cycle % from 0 - 100 */
-void forward(struct Motor *motors, int speed){
-		printf("Moving forward...");
-		softPwmWrite(motors -> enable_motor, speed);
-		digitalWrite(motors -> forward_motor, 0);
-		digitalWrite(motors -> reverse_motor, 1);
-}
-
-void reverse (struct Motor *motors, int speed){
-		printf("Moving in reverse...");
-		softPwmWrite(motors -> enable_motor, speed);
-		digitalWrite(motors -> forward_motor, 1);
-		digitalWrite(motors -> reverse_motor, 0);
-}
-void stop (struct Motor *motors) {
-		printf("Stopping...");
-		softPwmWrite(motors -> enable_motor, speed);
-		digitalWrite(motors -> forward_motor, 1);
-		digitalWrite(motors -> reverse_motor, 1);
-}
