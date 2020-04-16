ef4fc6862f4393952a0180db65f75d51c6bf01cc
nsainez <nsainez@gmail.com>
2020-04-15 20:25:05 -0700

    Motor*

--- assignment3.c
+++ assignment3.c
@@ -65,7 +65,7 @@ void stop(struct Motor *);
 }
 
 
-void init(struct Motor *motors, int enable, int forward, int reverse){
+void init(struct Motor * motors, int enable, int forward, int reverse){
 
 //	printf("Initializating motor %d: \n", motors);
 	motors->enable_motor = enable;
@@ -106,20 +106,20 @@ void init(struct Motor *motors, int enable, int forward, int reverse){
 	//}
 
 /* speed = duty cycle % from 0 - 100 */
-void forward(struct Motor *motors, int speed){
+void forward(struct Motor * motors, int speed){
 		printf("Moving forward...");
 		softPwmWrite(motors->enable_motor, speed);
 		digitalWrite(motors->forward_motor, 0);
 		digitalWrite(motors->reverse_motor, 1);
 }
 
-void reverse (struct Motor *motors, int speed){
+void reverse (struct Motor * motors, int speed){
 		printf("Moving in reverse...");
 		softPwmWrite(motors->enable_motor, speed);
 		digitalWrite(motors->forward_motor, 1);
 		digitalWrite(motors->reverse_motor, 0);
 }
-void stop (struct Motor *motors) {
+void stop (struct Motor * motors) {
 		printf("Stopping...");
 		softPwmWrite(motors->enable_motor, 0);
 		digitalWrite(motors->forward_motor, 1);
